b8d4327cc84823829d9c2bca6e969f8c48c2f23c
fdschepper <333476@student.mboutrecht.nl>
2023-04-05 12:18:51 +0200

    ALLES

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,81 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'determination'),
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Text(
-              "data",
-              style: TextStyle(
-                color: Colors.white,
-              ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Container(
+                      child: Text(
+                        "Frontend Developer",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromARGB(111, 205, 182, 111)),
+                    ),
+                    width: 500,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            "https://pbs.twimg.com/media/DoP2rEaX0AAXX58.jpg",
+                          ),
+                          colorFilter: ColorFilter.mode(
+                            Color.fromARGB(255, 150, 163, 169).withOpacity(0.8),
+                            BlendMode.modulate,
+                          )),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 200,
+                          right: 100,
+                        ),
+                        child: Text(
+                          "Game Developer",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 251, 250, 250),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(108, 242, 255, 237),
+                      ),
+                    ),
+                    width: 500,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            "http://3.bp.blogspot.com/-fSDv-1tzNkE/U6LhhQOOjxI/AAAAAAAAIpE/wPrW-FfTn4Q/s1600/1.jpg",
+                          ),
+                          colorFilter: ColorFilter.mode(
+                            Color.fromARGB(255, 221, 46, 46).withOpacity(0.8),
+                            BlendMode.modulate,
+                          )),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
